bf56052cfedda050516edd49b41e6ea3a28e06f1
Dimitri Simon <dimitri_simon@posteo.de>
2021-08-18 22:16:02 +0200

    implemented navigation from overview to detail fragment

---
+++ app/src/main/java/de/colognecode/superheroes/presentation/detail/SuperHeroDetailFragment.kt
@@ -0,0 +1,32 @@
+package de.colognecode.superheroes.presentation.detail
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import de.colognecode.superheroes.R
+
+class SuperHeroDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SuperHeroDetailFragment()
+    }
+
+    private lateinit var viewModel: SuperHeroDetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_super_hero_detail, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SuperHeroDetailViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
