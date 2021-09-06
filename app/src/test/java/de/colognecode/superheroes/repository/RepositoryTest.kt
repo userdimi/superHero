81e4393c1ff320eb95e597064ff8c8886f4d7c83
Dimitri Simon <dimitri_simon@posteo.de>
2021-09-06 22:05:41 +0200

    running but not working unit test

--- app/src/test/java/de/colognecode/superheroes/repository/RepositoryTest.kt
+++ app/src/test/java/de/colognecode/superheroes/repository/RepositoryTest.kt
@@ -1,41 +1,50 @@
 package de.colognecode.superheroes.repository
 
+import androidx.lifecycle.LiveData
 import de.colognecode.superheroes.di.apiModule
 import de.colognecode.superheroes.di.databaseModule
-import de.colognecode.superheroes.di.repositoryModule
 import de.colognecode.superheroes.repository.database.SuperHeroDao
+import de.colognecode.superheroes.repository.database.entities.SuperHero
 import de.colognecode.superheroes.repository.model.ResultsItem
 import de.colognecode.superheroes.repository.model.SuperHeroesResponse
 import de.colognecode.superheroes.repository.network.ApiKeyQuery
 import de.colognecode.superheroes.repository.network.SuperHeroesApi
 import io.mockk.*
-import org.junit.Rule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
-import org.koin.core.logger.Level
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.RegisterExtension
 import org.koin.test.KoinTest
-import org.koin.test.KoinTestRule
-import org.koin.test.mock.MockProviderRule
+import org.koin.test.inject
+import org.koin.test.junit5.KoinTestExtension
+import org.koin.test.junit5.mock.MockProviderExtension
 import org.koin.test.mock.declareMock
 import retrofit2.Response
 
 internal class RepositoryTest : KoinTest {
 
-    @get:Rule
-    val koinTestRule = KoinTestRule.create {
-        printLogger(Level.DEBUG)
-        modules(apiModule)
-        modules(databaseModule)
-        modules(repositoryModule)
+    private val mockSuperHeroesApi: SuperHeroesApi by inject()
+    private val mockSuperHeroDao: SuperHeroDao by inject()
+    private val mockApiKeyQuery: ApiKeyQuery by inject()
+
+    @JvmField
+    @RegisterExtension
+    val koinTestExtension = KoinTestExtension.create {
+        modules(
+            apiModule,
+            databaseModule
+        )
     }
 
-    @get:Rule
-    val mockProvider = MockProviderRule.create { clazz ->
-        mockkClass(clazz.java.kotlin)
+    @JvmField
+    @RegisterExtension
+    val mockProvider = MockProviderExtension.create { clazz ->
+        mockkClass(clazz)
     }
 
-    @Test
-    fun `Verify that super heroes are saved into the database on success fetch`() {
-        // arrange
+    @BeforeEach
+    fun initMocks() {
         val mockResultItem = declareMock<ResultsItem> {
             every { id } returns "12345"
             every { name } returns "Hulk"
@@ -47,7 +56,10 @@ internal class RepositoryTest : KoinTest {
             every { body()?.data?.results } returns mockResultsItemList
         }
         val mockApiKeyQuery = mockk<ApiKeyQuery>()
-        val mockSuperHeroDao = mockk<SuperHeroDao>()
+        val mockLiveData = mockk<LiveData<List<SuperHero>>>()
+        val mockSuperHeroDao = declareMock<SuperHeroDao> {
+            every { getAllSuperHeroes() } returns mockLiveData
+        }
         val mockSuperHeroApi = declareMock<SuperHeroesApi> {
             coEvery {
                 fetchHeroes(
@@ -57,16 +69,20 @@ internal class RepositoryTest : KoinTest {
                 )
             } returns mockResponse
         }
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `Verify that super heroes are saved into the database on success fetch`() {
         // act
-        Repository(mockSuperHeroApi, mockApiKeyQuery, mockSuperHeroDao)
-        // assert
-        coVerify {
-            mockSuperHeroApi.fetchHeroes(
-                mockApiKeyQuery.timestamp,
-                mockApiKeyQuery.publicApiKey,
-                mockApiKeyQuery.apiKeyHash
-            )
+        runBlockingTest {
+            Repository(
+                mockSuperHeroesApi,
+                mockApiKeyQuery,
+                mockSuperHeroDao
+            ).fetchSuperHeroesFromApi()
         }
+        // assert
         coVerify { mockSuperHeroDao.addSuperHero(any()) }
     }
 }
